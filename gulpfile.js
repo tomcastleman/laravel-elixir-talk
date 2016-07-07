745be0cf4b4cb4dda35749185ac394998d421c55
Tom Castleman <tom@b3it.co>
2016-07-07 16:52:55 +0100

    working example

--- gulpfile.js
+++ gulpfile.js
@@ -22,4 +22,18 @@ elixir(function (mix) {
     'public/build/*',
   ]);
 
+  mix.sass('app.scss');
+  
+  mix.webpack('app.js');
+
+  // copy fonts
+  var fonts = [
+    'node_modules/bootstrap-sass/assets/fonts',
+    'node_modules/font-awesome/fonts'
+  ];
+  for(var f in fonts) {
+    mix.copy(fonts[f], 'public/fonts');
+    mix.copy(fonts[f], 'public/build/fonts');
+  }
+
 });
